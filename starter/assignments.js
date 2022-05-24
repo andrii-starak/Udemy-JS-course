60ab2b96c0fa344ef2cf11f1c7e104d9f716ed50
andrii-starak <andrii.starak6@gmail.com>
2022-05-24 15:07:13 +0300

    1-17 lessons

---
+++ starter/assignments.js
@@ -0,0 +1,55 @@
+// alert("Hello World!");
+
+// let js = 'amazing'
+// --------------------------------------------------
+/*
+
+let country = "Ukraine";
+const continent = "Europe";
+let population = "40";
+
+console.log(country, continent, population);
+const isIsland = false;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+const language = "ukranian";
+
+console.log(population / 2);
+population++;
+console.log(population);
+
+let fin = 6;
+console.log(population > fin);
+
+const average = 33;
+
+console.log(population < average);
+
+let description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+
+// console.log(country + " is in " + continent + ", and its " + population + " million people speak " + language);
+console.log(description);
+___________________________________________________________
+
+// BMI = mass / height ** 2 = mass / (height * height); //(mass in kg and height in meter)
+
+// const markMass = 78;
+// const johnMass = 92;
+// const markHeight = 1.69;
+// const johnHeight = 1.95;
+
+const markMass = 95;
+const johnMass = 85;
+const markHeight = 1.88;
+const johnHeight = 1.76;
+
+let markBMI = markMass / markHeight ** 2;
+let johnBMI = johnMass / johnHeight ** 2;
+
+console.log(markBMI, johnBMI);
+let markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+// */
---
+++ starter/script2.js
@@ -0,0 +1,24 @@
+/*
+let javascriptIsFun = true;
+
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun ="YES!;"
+console.log(javascriptIsFun);
+
+const now = 2037;
+const ageSarah = now - 2020;
+
+console.log(ageSarah >= 18);
+const isFullAge = ageSarah >= 18;
+
+console.log(isFullAge);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+*/
